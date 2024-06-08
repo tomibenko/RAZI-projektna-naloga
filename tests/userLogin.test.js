76f68bd137324d005987fd721329cd3a2d240d41
zanlukakralj <zan.kralj3@student.um.si>
2024-06-08 21:03:59 +0200

    Popravek workflow app

--- tests/userLogin.test.js
+++ tests/userLogin.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../backend/app');
 const { expect } = require('chai');
 
 describe('User Login Workflow', () => {
